82dac98aa0c844654e7e731296a129dfd1c10019
Sergio Vegas Alonso <svegas@bilbomatica.es>
2023-11-02 11:45:58 +0100

    Library ok.

---
+++ dist-browser/index.js
@@ -0,0 +1,21 @@
+/**
+ * Adds two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @returns Result.
+ */
+export var add = function (a, b) { return a + b; };
+/**
+ * Subtracts two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @returns Result.
+ */
+export var subtract = function (a, b) { return a - b; };
+/**
+ * Multiplies two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @returns Result.
+ */
+export var multiply = function (a, b) { return a * b; };
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Adds two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @returns Result.
+ */
+export const add = (a: number, b: number): number => a + b;
+
+/**
+ * Subtracts two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @returns Result.
+ */
+export const subtract = (a: number, b: number): number => a - b;
+
+/**
+ * Multiplies two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @returns Result.
+ */
+export const multiply = (a: number, b: number): number => a * b;
