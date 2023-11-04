d66761158ca6b921be913f57b214ff752273d6b1
Sergio Vegas Alonso <svegas@bilbomatica.es>
2023-11-04 20:50:28 +0100

    New policies

--- dist/browser/index.js
+++ dist/browser/index.js
@@ -4,24 +4,18 @@
  * @param b Second number.
  * @returns Result.
  */
-export var add = function (a, b) {
-  return a + b
-}
+export var add = function (a, b) { return a + b; };
 /**
  * Subtracts two numbers.
  * @param a First number.
  * @param b Second number.
  * @returns Result.
  */
-export var subtract = function (a, b) {
-  return a - b
-}
+export var subtract = function (a, b) { return a - b; };
 /**
  * Multiplies two numbers.
  * @param a First number.
  * @param b Second number.
  * @returns Result.
  */
-export var multiply = function (a, b) {
-  return a * b
-}
+export var multiply = function (a, b) { return a * b; };
